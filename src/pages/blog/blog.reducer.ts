b9ce4ac38dc512a55733a45817375f08a227f21e
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-01 14:39:06 +0700

    handled action addPost into Reducer with builder callback

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -1,4 +1,4 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 import { initialPostList } from 'constants/blog'
 import { Post } from 'types/blog.type'
 // createReducer : nó nhận tham số đầu tiên là initial State
@@ -12,8 +12,20 @@ const initialState = {
   postList: initialPostList
 }
 
+// createAction là một helper function dùng để tạo một Redux Action
+// function createAction (type , prepareAction?)
+// mỗi cái action đều phải có 1 cái type
+export const addPost = createAction<Post>('blog/addPost')
+
 // cái builder callBack là nơi mà chúng ta  sẽ xử lí những cái action
 // và cập nhập state trong này
-const blogReducer = createReducer(initialState, (builder) => {})
+const blogReducer = createReducer(initialState, (builder) => {
+  builder.addCase(addPost, (state, action) => {
+    // immmerjs
+    // immerjs giúp chúng ta mutate một state an toàn
+    const post = action.payload
+    state.postList.push(post)
+  })
+})
 
 export default blogReducer
--- src/pages/blog/components/CreatePost/CreatePost.tsx
+++ src/pages/blog/components/CreatePost/CreatePost.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { addPost } from 'pages/blog/blog.reducer'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Post } from 'types/blog.type'
+
+const initialState: Post = {
+  id: '',
+  description: '',
+  featuredImage: '',
+  publishDate: '',
+  published: false,
+  title: ''
+}
 
 export default function CreatePost() {
+  // mình nên tạo 1 cái state tại state này chỉ dùng trong component này nên không bỏ vào redux
+  const [formData, setFormData] = useState<Post>(initialState)
+  // nếu đưa id vào redux ta dùng dispatch
+  const dispatch = useDispatch()
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formDataWithId = { ...formData, id: new Date().toISOString() }
+    dispatch(addPost(formDataWithId))
+    console.log(formData)
+  }
+  // khi nhấn submit khi dispatch 1 cái action tên là addPost
+  // khi dispatch action thì reducer kiểm tra thấy có action dispatch rồi
+  // builder thực hiện và cập nhập lại state
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='mb-6'>
         <label htmlFor='title' className='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
           Title
@@ -13,6 +38,8 @@ export default function CreatePost() {
           className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500'
           placeholder='Title'
           required
+          value={formData.title}
+          onChange={(event) => setFormData((prev) => ({ ...prev, title: event.target.value }))}
         />
       </div>
       <div className='mb-6'>
@@ -25,6 +52,8 @@ export default function CreatePost() {
           className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500'
           placeholder='Url image'
           required
+          value={formData.featuredImage}
+          onChange={(event) => setFormData((prev) => ({ ...prev, featuredImage: event.target.value }))}
         />
       </div>
       <div className='mb-6'>
@@ -38,7 +67,8 @@ export default function CreatePost() {
             className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500'
             placeholder='Your description...'
             required
-            defaultValue={''}
+            value={formData.description}
+            onChange={(event) => setFormData((prev) => ({ ...prev, description: event.target.value }))}
           />
         </div>
       </div>
@@ -52,10 +82,18 @@ export default function CreatePost() {
           className='block w-56 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:outline-none focus:ring-blue-500'
           placeholder='Title'
           required
+          value={formData.publishDate}
+          onChange={(event) => setFormData((prev) => ({ ...prev, publishDate: event.target.value }))}
         />
       </div>
       <div className='mb-6 flex items-center'>
-        <input id='publish' type='checkbox' className='h-4 w-4 focus:ring-2 focus:ring-blue-500' />
+        <input
+          id='publish'
+          type='checkbox'
+          checked={formData.published}
+          onChange={(event) => setFormData((prev) => ({ ...prev, published: event.target.checked }))}
+          className='h-4 w-4 focus:ring-2 focus:ring-blue-500'
+        />
         <label htmlFor='publish' className='ml-2 text-sm font-medium text-gray-900'>
           Publish
         </label>
@@ -69,7 +107,7 @@ export default function CreatePost() {
             Publish Post
           </span>
         </button>
-        <button
+        {/* <button
           type='submit'
           className='group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-lime-200 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 dark:focus:ring-lime-800'
         >
@@ -84,7 +122,7 @@ export default function CreatePost() {
           <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
             Cancel
           </span>
-        </button>
+        </button> */}
       </div>
     </form>
   )
