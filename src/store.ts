9d4f0a736b7694c21e3ed5633354982645dee462
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-01 11:11:55 +0700

    update

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import blogReducer from 'pages/blog/blog.reducer'
+// store là một object chứa state tree . Chỉ nên có duy nhất 1 store trong app
+// store sẽ chạy rootReducer bất cứ khi nào một action được dispatch
+// dispatch : là một function dùng để gửi một action đi đến store (hay còn gọi là reducer cũng được)
+// reducer : là một function  để tính toán state mới dựa vào previous state + action
+// action : là một plain object (object đơn giản tạo bằng {} hoặc newObject ) chứa field là type mô tả
+// chuyện gì vừa xảy ra , action là một object nhưng chúng ta thường khai báo một function return về
+// object để có thể dễ dàng gửi data vào action , chúng ta thường gọi là payload
 
 // function configureStore nó sẽ generate cho chúng ta cái store
 // nhận 1 object
