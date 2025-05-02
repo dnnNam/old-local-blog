18f114a87bd211156e2082924553bee20191dd98
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-02 10:13:20 +0700

    delete feature

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -8,7 +8,7 @@ interface BlogSate {
   postList: Post[]
 }
 
-const initialState = {
+const initialState: BlogSate = {
   postList: initialPostList
 }
 
@@ -17,15 +17,25 @@ const initialState = {
 // mỗi cái action đều phải có 1 cái type
 export const addPost = createAction<Post>('blog/addPost')
 
+// chức năng xóa tìm ra index và dùng splice để xóa
+export const detelePost = createAction<String>('blog/detelePost')
 // cái builder callBack là nơi mà chúng ta  sẽ xử lí những cái action
 // và cập nhập state trong này
 const blogReducer = createReducer(initialState, (builder) => {
-  builder.addCase(addPost, (state, action) => {
-    // immmerjs
-    // immerjs giúp chúng ta mutate một state an toàn
-    const post = action.payload
-    state.postList.push(post)
-  })
+  builder
+    .addCase(addPost, (state, action) => {
+      // immmerjs
+      // immerjs giúp chúng ta mutate một state an toàn
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(detelePost, (state, action) => {
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    })
 })
 
 export default blogReducer
