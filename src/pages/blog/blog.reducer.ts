655947a31fd6ae6547fa9b4970e4d3e2e8527a10
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-02 15:33:10 +0700

    feature editPost

--- src/pages/blog/blog.reducer.ts
+++ src/pages/blog/blog.reducer.ts
@@ -6,10 +6,12 @@ import { Post } from 'types/blog.type'
 
 interface BlogSate {
   postList: Post[]
+  editingPost: Post | null
 }
 
 const initialState: BlogSate = {
-  postList: initialPostList
+  postList: initialPostList,
+  editingPost: null
 }
 
 // createAction là một helper function dùng để tạo một Redux Action
@@ -19,6 +21,8 @@ export const addPost = createAction<Post>('blog/addPost')
 
 // chức năng xóa tìm ra index và dùng splice để xóa
 export const detelePost = createAction<String>('blog/detelePost')
+// chức năng update
+export const startEditingPost = createAction<string>('blog/startEditingPost')
 // cái builder callBack là nơi mà chúng ta  sẽ xử lí những cái action
 // và cập nhập state trong này
 const blogReducer = createReducer(initialState, (builder) => {
@@ -35,6 +39,12 @@ const blogReducer = createReducer(initialState, (builder) => {
       if (foundPostIndex !== -1) {
         state.postList.splice(foundPostIndex, 1)
       }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const postId = action.payload
+      // nếu tìm không thấy sẽ là null
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
     })
 })
 
