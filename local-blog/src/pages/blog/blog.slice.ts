ff383423e4218e255ded4f472019dc82df1eb3a2
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-03 18:55:06 +0700

    createSlice

---
+++ local-blog/src/pages/blog/blog.slice.ts
@@ -0,0 +1,133 @@
+import { createAction, createReducer, current, nanoid, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { initialPostList } from 'constants/blog'
+import { Post } from 'types/blog.type'
+// createReducer : nó nhận tham số đầu tiên là initial State
+// cái thứ 2 là 1 cái builder callback
+
+interface BlogSate {
+  postList: Post[]
+  editingPost: Post | null
+}
+
+const initialState: BlogSate = {
+  postList: initialPostList,
+  editingPost: null
+}
+
+// createAction là một helper function dùng để tạo một Redux Action
+// function createAction (type , prepareAction?)
+// mỗi cái action đều phải có 1 cái type
+
+// đề mô prepare callback cho 1 bài blog có id là nanoid
+// export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
+//   return {
+//     payload: {
+//       ...post,
+//       id: nanoid()
+//     }
+//   }
+// })
+
+// // chức năng xóa tìm ra index và dùng splice để xóa
+// export const detelePost = createAction<String>('blog/detelePost')
+// // chức năng update
+// export const startEditingPost = createAction<string>('blog/startEditingPost')
+// export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+// // xử lí button cancel
+// export const cancelEditingPost = createAction('blog/ cancelEditingPost')
+
+const blogSlice = createSlice({
+  name: 'blog',
+  initialState: initialState,
+  reducers: {
+    detelePost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    },
+    startEditingPost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      // nếu tìm không thấy sẽ là null
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    },
+    cancelEditingPost: (state) => {
+      state.editingPost = null
+    },
+    finishEditingPost: (state, action: PayloadAction<Post>) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    },
+    addPost: {
+      reducer: (state, action: PayloadAction<Post>) => {
+        // immmerjs
+        // immerjs giúp chúng ta mutate một state an toàn
+        const post = action.payload
+        state.postList.push(post)
+      },
+      prepare: (post: Omit<Post, 'id'>) => ({
+        payload: {
+          ...post,
+          id: nanoid()
+        }
+      })
+    }
+  }
+})
+
+export const { addPost, cancelEditingPost, detelePost, finishEditingPost, startEditingPost } = blogSlice.actions
+const blogReducer = blogSlice.reducer
+export default blogReducer
+// const blogReducer = createReducer(initialState, (builder) => {
+//   builder
+//     .addCase(addPost, (state, action) => {
+//       // immmerjs
+//       // immerjs giúp chúng ta mutate một state an toàn
+//       const post = action.payload
+//       state.postList.push(post)
+//     })
+//     .addCase(detelePost, (state, action) => {
+//       const postId = action.payload
+//       const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+//       if (foundPostIndex !== -1) {
+//         state.postList.splice(foundPostIndex, 1)
+//       }
+//     })
+//     .addCase(startEditingPost, (state, action) => {
+//       const postId = action.payload
+//       // nếu tìm không thấy sẽ là null
+//       const foundPost = state.postList.find((post) => post.id === postId) || null
+//       state.editingPost = foundPost
+//     })
+//     .addCase(cancelEditingPost, (state) => {
+//       state.editingPost = null
+//     })
+//     .addCase(finishEditingPost, (state, action) => {
+//       const postId = action.payload.id
+//       state.postList.some((post, index) => {
+//         if (post.id === postId) {
+//           state.postList[index] = action.payload
+//           return true
+//         }
+//         return false
+//       })
+//       state.editingPost = null
+//     })
+//     .addMatcher(
+//       (action) => action.type.includes('cancel'),
+//       (state, action) => {
+//         console.log(current(state))
+//       }
+//     )
+// })
+
+// export default blogReducer
---
+++ old-local-blog/src/types/blog.type.ts
@@ -0,0 +1,10 @@
+// định nghĩa một interface (giao diện)
+// dùng mô tả một bài viết (blog)
+export interface Post {
+  title: string // tiêu đề bài viết
+  description: string //mô tả ngắn gọn bài viết
+  publishDate: string // ngày đăng bài
+  id: string // mã định danh duy nhất
+  featuredImage: string // đường dẫn ảnh đại diện cho bài viết
+  published: boolean // trạng thái bài viết true là xuất bản , false là bản nháp
+}
