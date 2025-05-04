1fd45b576886b85812768cf75759eb51b9b7104d
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-04 20:18:08 +0700

    createAsyncThunk

---
+++ remote-blog/src/pages/blog/blog.reducer.ts
@@ -0,0 +1,93 @@
+import { createAction, createReducer, current, nanoid } from '@reduxjs/toolkit'
+import { initialPostList } from 'constants/blog'
+import { Post } from 'types/blog.type'
+// createReducer : nó nhận tham số đầu tiên là initial State
+// cái thứ 2 là 1 cái builder callback
+
+interface BlogSate {
+  postList: Post[]
+  editingPost: Post | null
+}
+
+const initialState: BlogSate = {
+  postList: initialPostList,
+  editingPost: null
+}
+
+// createAction là một helper function dùng để tạo một Redux Action
+// function createAction (type , prepareAction?)
+// mỗi cái action đều phải có 1 cái type
+
+// đề mô prepare callback cho 1 bài blog có id là nanoid
+export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
+  return {
+    payload: {
+      ...post,
+      id: nanoid()
+    }
+  }
+})
+
+// chức năng xóa tìm ra index và dùng splice để xóa
+export const detelePost = createAction<String>('blog/detelePost')
+// chức năng update
+export const startEditingPost = createAction<string>('blog/startEditingPost')
+export const finishEditingPost = createAction<Post>('blog/finishEditingPost')
+// xử lí button cancel
+export const cancelEditingPost = createAction('blog/ cancelEditingPost')
+// cái builder callBack là nơi mà chúng ta  sẽ xử lí những cái action
+// và cập nhập state trong này
+const blogReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addPost, (state, action) => {
+      // immmerjs
+      // immerjs giúp chúng ta mutate một state an toàn
+      const post = action.payload
+      state.postList.push(post)
+    })
+    .addCase(detelePost, (state, action) => {
+      const postId = action.payload
+      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+      if (foundPostIndex !== -1) {
+        state.postList.splice(foundPostIndex, 1)
+      }
+    })
+    .addCase(startEditingPost, (state, action) => {
+      const postId = action.payload
+      // nếu tìm không thấy sẽ là null
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    })
+    .addCase(cancelEditingPost, (state) => {
+      state.editingPost = null
+    })
+    .addCase(finishEditingPost, (state, action) => {
+      const postId = action.payload.id
+      state.postList.some((post, index) => {
+        if (post.id === postId) {
+          state.postList[index] = action.payload
+          return true
+        }
+        return false
+      })
+      state.editingPost = null
+    })
+    .addMatcher(
+      (action) => action.type.includes('cancel'),
+      (state, action) => {
+        console.log(current(state))
+      }
+    )
+})
+
+// ngoài cách dùng builder callBack còn có mapObject nhưng nó không ổn định với typeScript
+// vì khi dùng mapObject sẽ mất định nghĩa chuyển thành any phải định nghĩa lại
+
+export default blogReducer
+// nếu như log state trong reducer thì sẽ in ra proxy khó đọc (đó là draft state  trong immerjs)
+// muốn xem state thì phải dùng thêm thằng current để in ra
+// ngoài những addCase ra còn cách dùng như
+// addMatcher cho phép chúng ta truyền vào addMatcher function
+// khi mà addMatcher return true thì chạy cái callback
+// chuyên xử lí những cái state động
+// ngoài ra còn có addDefaultCase khi không nhảy case thì nó nhảy vào defaultCase
---
+++ remote-blog/src/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit'
+import blogReducer from 'pages/blog/blog.reducer'
+// store là một object chứa state tree . Chỉ nên có duy nhất 1 store trong app
+// store sẽ chạy rootReducer bất cứ khi nào một action được dispatch
+// dispatch : là một function dùng để gửi một action đi đến store (hay còn gọi là reducer cũng được)
+// reducer : là một function  để tính toán state mới dựa vào previous state + action
+// action : là một plain object (object đơn giản tạo bằng {} hoặc newObject ) chứa field là type mô tả
+// chuyện gì vừa xảy ra , action là một object nhưng chúng ta thường khai báo một function return về
+// object để có thể dễ dàng gửi data vào action , chúng ta thường gọi là payload
+
+// function configureStore nó sẽ generate cho chúng ta cái store
+// nhận 1 object
+export const store = configureStore({
+  reducer: { blog: blogReducer }
+})
+
+// lấy RootState và App Dispatch từ store của chúng ta
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+// 2 thằng trên phục vụ cho vấn đề typescript
+// những cái state nào các bạn nhiều component khác nhau thì đặt ở redux
+// hay gọi là reducer của thằng redux
+// còn state nào chỉ dùng cho component của bạn thôi cứ dùng cái state react bình thường
+// không cần dùng redux làm gì cả (nhớ nha)
