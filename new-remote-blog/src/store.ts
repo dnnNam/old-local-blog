f71339f953b00df7a8ed9ad8cc31474645b99957
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-05-08 21:08:00 +0700

    integrated reducer created from RTK into store

--- new-remote-blog/src/store.ts
+++ new-remote-blog/src/store.ts
@@ -1,13 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { blogApi } from 'pages/blog/blog.service'
 import blogReducer from 'pages/blog/blog.slice'
 // ...
 
 export const store = configureStore({
   reducer: {
-    blog: blogReducer
-  }
+    blog: blogReducer,
+    [blogApi.reducerPath]: blogApi.reducer
+  },
+  // thêm api middleware để enable các tính năng như caching , invalidation , polling của rtk-query
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(blogApi.middleware)
 })
 
+// optional , nhưng bắt buộc nếu dùng tính năng refetchOnFocus / refetchOnReconnect
+setupListeners(store.dispatch)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
